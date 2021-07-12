294d2cf0d9cd0e2e8c75cb3571e95e5d6e358173
Fliteska <fliteska@gmail.com>
2021-07-12 17:34:06 +0100

    initial

---
+++ src/bot.js
@@ -0,0 +1,37 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+require("dotenv").config();
+function scpRegexMatches(content) {
+    const regex = /SCP-\d{1,4}/gim;
+    let m;
+    const scpList = [];
+
+    while ((m = regex.exec(content)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        m.forEach((match, groupIndex) => {
+            scpList.push(match.toLowerCase());
+        });
+    }
+    return scpList;
+}
+client.on("ready", () => {
+    console.log(`${client.user.tag} is up and running.`);
+});
+
+client.on("message", async ({ channel, content }) => {
+    const scpMentions = scpRegexMatches(content);
+    if (scpMentions.length === 0) {
+        return;
+    }
+    scpMentions.forEach(scp => {
+        channel.send(`https://scpwiki.com/${scp}`);
+    });
+});
+
+try {
+    client.login(process.env.BOT_TOKEN);
+} catch (error) {
+    console.log(`There is an error in token ${error}`);
+}
